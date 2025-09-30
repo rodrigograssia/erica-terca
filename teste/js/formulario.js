cd30b5348cddf0af019f576a3a7abba8f2078c11
Rodrigo Oliveira Grassia <824135663@ulife.com.br>
2025-09-30 09:34:43 -0300

    Organizar arquivos

--- teste/js/formulario.js
+++ teste/js/formulario.js
@@ -16,34 +16,3 @@ function undoAction() {
 function redoAction() {
     showConfirmButton('redo');
 }
-
-function showConfirmButton(action) {
-    let confirmBtn = document.getElementById('confirm-btn');
-    if (!confirmBtn) {
-        confirmBtn = document.createElement('button');
-        confirmBtn.id = 'confirm-btn';
-        confirmBtn.type = 'button';
-        confirmBtn.textContent = 'Confirmar ação';
-        confirmBtn.style.marginLeft = '10px';
-        document.querySelector('.toolbar').appendChild(confirmBtn);
-    }
-    confirmBtn.style.display = 'inline-block';
-    confirmBtn.onclick = function() {
-        if (action === 'undo') {
-            if (undoStack.length > 0) {
-                redoStack.push(editor.value);
-                const lastState = undoStack.pop();
-                editor.value = lastState;
-            } else {
-                editor.value = '';
-            }
-        } else if (action === 'redo') {
-            if (redoStack.length > 0) {
-                undoStack.push(editor.value);
-                const nextState = redoStack.pop();
-                editor.value = nextState;
-            }
-        }
-        confirmBtn.style.display = 'none';
-    };
-}
